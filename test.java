7adff69bf33e070bbb984ec36377700887f91bdf
Roman Chametka <roman.2k5@gmail.com>
2016-05-24 17:46:53 -0400

    ...

--- test.java
+++ test.java
@@ -1,15 +1,7 @@
 public class test {
 
     public static void main(String[] args) {
-        System.out.println("hotfix is completed!");
+        System.out.println("testing");
     }
-	public void whatever()
-	{
-		System.out.println("this is a new method we forgot to add before.");
-	}
-	public void anotherMethod()
-	{
-		System.out.println("I don't understand how this works...");
-	}
 
 }
