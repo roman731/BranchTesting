ac04f768772c0f962511de516832df0940fa17d1
Roman Chametka <roman.2k5@gmail.com>
2016-05-24 17:44:14 -0400

    added another method

--- test.java
+++ test.java
@@ -3,10 +3,12 @@ public class test {
     public static void main(String[] args) {
         System.out.println("UMM HELLO!");
     }
-
 	public void whatever()
 	{
 		System.out.println("this is a new method we forgot to add before.");
-		
+	}
+	public void anotherMethod()
+	{
+		System.out.println("I don't understand how this works...");
 	}
 }
