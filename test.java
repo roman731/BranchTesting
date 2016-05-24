a751ed913e8d38e9fe8f833816ca0a8899b3cf92
Roman Chametka <roman.2k5@gmail.com>
2016-05-24 17:23:23 -0400

    removed method from thirdBranch

--- test.java
+++ test.java
@@ -3,10 +3,5 @@ public class test {
     public static void main(String[] args) {
         System.out.println("test again");
     }
-	
-	public void newMethod()
-	{
-		System.out.println("This is a method inside the test class.");
-	}
 
 }
