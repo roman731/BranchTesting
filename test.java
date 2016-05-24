ee582cd6bf503885af6e23c79a2c381e241777ac
Roman Chametka <roman.2k5@gmail.com>
2016-05-24 17:40:10 -0400

    not working

--- test.java
+++ test.java
@@ -1,7 +1,7 @@
 public class test {
 
     public static void main(String[] args) {
-        System.out.println("hotfix complete!");
+        System.out.println("UMM HELLO!");
     }
 
 	public void whatever()
