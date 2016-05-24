753a2f263ac5c962e671e4504dd1279261f5db5c
Roman Chametka <roman.2k5@gmail.com>
2016-05-24 17:58:58 -0400

    applied hotfix

--- test.java
+++ test.java
@@ -1,7 +1,8 @@
 public class test {
 
     public static void main(String[] args) {
-        System.out.println("testing");
+        System.out.println("hotfixed this text!");
+        System.out.println("done!");
     }
 
 }
