f61ed0ee19d85aa6c46f80dec2482253967ba9bf
Roman Chametka <roman.2k5@gmail.com>
2016-05-24 17:33:22 -0400

    going back

--- test.java
+++ test.java
@@ -1,12 +1,7 @@
 public class test {
 
     public static void main(String[] args) {
-        System.out.println("hotfix complete!");
+        System.out.println("testing");
     }
 
-	public void whatever()
-	{
-		System.out.println("this is a new method we forgot to add before.");
-		
-	}
 }
