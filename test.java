14191c5c7073473d8aa12a8244597fb9aeef3039
Roman Chametka <roman.2k5@gmail.com>
2016-05-24 16:56:51 -0400

    testing more

--- test.java
+++ test.java
@@ -1,11 +1,7 @@
 public class test {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("test again");
-=======
-        System.out.println("this is the third branch");
->>>>>>> thirdBranch
     }
 
 }
