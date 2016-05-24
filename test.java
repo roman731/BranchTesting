248985b670f623457b31fc39f1a13da5d45d8d06
Roman Chametka <roman.2k5@gmail.com>
2016-05-24 17:30:50 -0400

    hotfix57 completed

--- test.java
+++ test.java
@@ -1,7 +1,12 @@
 public class test {
 
     public static void main(String[] args) {
-        System.out.println("test again");
+        System.out.println("hotfix complete!");
     }
 
+	public void whatever()
+	{
+		System.out.println("this is a new method we forgot to add before.");
+		
+	}
 }
