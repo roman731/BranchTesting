03da33a81cee1ae76aadbc5e53161f1487ce16f2
Roman Chametka <roman.2k5@gmail.com>
2016-05-24 17:01:29 -0400

    changed main method to be same as master, added new method

--- test.java
+++ test.java
@@ -1,7 +1,7 @@
 public class test {
 
     public static void main(String[] args) {
-        System.out.println("this is the third branch");
+        System.out.println("test again");
     }
 	
 	public void newMethod()
