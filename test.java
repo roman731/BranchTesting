e6a0e62dc1fcc5876260a7e791fa971c879dc3f6
Roman Chametka <roman.2k5@gmail.com>
2016-05-24 16:59:31 -0400

    added a method that doesn't exist in master branch

--- test.java
+++ test.java
@@ -3,5 +3,10 @@ public class test {
     public static void main(String[] args) {
         System.out.println("this is the third branch");
     }
+	
+	public void newMethod()
+	{
+		System.out.println("This is a method inside the test class.");
+	}
 
 }
