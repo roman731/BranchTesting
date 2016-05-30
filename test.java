12f9abbcc5c01ec7354c3cc7811260ef8d497ef8
Roman Chametka <roman.2k5@gmail.com>
2016-05-30 17:51:26 -0400

    completed hotfix

--- test.java
+++ test.java
@@ -1,7 +1,7 @@
 public class test {
 
     public static void main(String[] args) {
-        System.out.println("Hey this is test 3");
+        System.out.println("This is the hotfix! We needed to change this text!");
     }
 	
 
