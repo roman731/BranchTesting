d85109a3ce760a82d562a832307c0ebe53f05293
Roman Chametka <roman.2k5@gmail.com>
2016-05-30 18:31:51 -0400

    made changes

--- test.java
+++ test.java
@@ -3,10 +3,4 @@ public class test {
     public static void main(String[] args) {
         System.out.println("test");
     }
-		
-	public void newMethod()
-	{
-		System.out.println("working on new stuff!");
-	}
-
 }
