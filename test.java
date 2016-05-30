e5fdcf13f51a8ce0d87d175f123c7f28b468f1bf
Roman Chametka <roman.2k5@gmail.com>
2016-05-30 18:23:52 -0400

    more stuff

--- test.java
+++ test.java
@@ -3,12 +3,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("applied the hotfix! It is done!");
     }
-	
-	public void newStuffMethod()
-	{
-		System.out.println("Added some code. We are done with the new stuff feature.");
-	}
-	
+		
 	public void everMoreNewStuff()
 	{
 		System.out.println("TEST");
