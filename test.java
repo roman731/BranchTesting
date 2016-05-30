46f10d249a0baae5528cd9a96ae338703211d4dd
Roman Chametka <roman.2k5@gmail.com>
2016-05-30 18:06:12 -0400

    fixed master

--- test.java
+++ test.java
@@ -1,12 +1,8 @@
 public class test {
 
     public static void main(String[] args) {
-        System.out.println("This is the hotfix! We needed to change this text!");
+        System.out.println("test");
     }
-	
-	public void newStory()
-	{
-		System.out.println("Some new text here I'm working on. It's currently in progress. Some more stuff.");
-	}
+
 
 }
