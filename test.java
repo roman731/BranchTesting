5e037fa1c110afa96a946a05e9b1efbd32477f45
Roman Chametka <roman.2k5@gmail.com>
2016-05-30 18:33:07 -0400

    started working on new feature

--- test.java
+++ test.java
@@ -3,4 +3,9 @@ public class test {
     public static void main(String[] args) {
         System.out.println("test");
     }
+	
+	public void newFeature()
+	{
+		System.out.println("started working on new feature");
+	}
 }
