d0e7392ef987e41763dbf90580543600b16955b2
Roman Chametka <roman.2k5@gmail.com>
2016-05-30 17:49:35 -0400

    started working on new story. Done for the day

--- test.java
+++ test.java
@@ -4,5 +4,9 @@ public class test {
         System.out.println("Hey this is test 3");
     }
 	
+	public void newStory()
+	{
+		System.out.println("Some new text here I'm working on. It's currently in progress.");
+	}
 
 }
