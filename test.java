6ad38b396f41cb6eb8e32989febb22822361764e
Roman Chametka <roman.2k5@gmail.com>
2016-05-30 18:44:09 -0400

    default

--- test.java
+++ test.java
@@ -1,11 +1,6 @@
 public class test {
 
     public static void main(String[] args) {
-        System.out.println("the hotfix is done!");
+        System.out.println("test");
     }
-	
-	public void newFeature()
-	{
-		System.out.println("started working on new feature. Finished working on new feature.");
-	}
 }
